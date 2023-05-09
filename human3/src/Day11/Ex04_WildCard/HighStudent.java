448da65cef2dae563e8e0bbb96d77d1ddd0c3bdd
Ssseyoung <131742093+Ssseyoung@users.noreply.github.com>
2023-05-09 15:48:51 +0900

    Delete human3 directory

--- human3/src/Day11/Ex04_WildCard/HighStudent.java
+++
@@ -1,9 +0,0 @@
-package Day11.Ex04_WildCard;
-
-public class HighStudent extends Student {
-
-	public HighStudent(String name) {
-		super(name);
-	}
-
-}
--- human3/src/Day11/Ex04_WildCard/Student.java
+++
@@ -1,9 +0,0 @@
-package Day11.Ex04_WildCard;
-
-public class Student extends Person {
-
-	public Student(String name) {
-		super(name);
-	}
-
-}
